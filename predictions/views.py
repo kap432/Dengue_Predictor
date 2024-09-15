c3adcfe04af3cddf941af42b600b2295343160a5
Harsh Kapse <kapseharsht@gmail.com>
2024-09-15 12:52:06 +0530

    RF_implemented

--- predictions/views.py
+++ predictions/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-import pickle
+import joblib
 import numpy as np
+from django.http import JsonResponse
 
 def landing_page(request):
     return render(request, 'predictions/index.html')
@@ -11,13 +12,14 @@ def dengue_symptoms(request):
 def predict_view(request):
     return render(request, 'predictions/index.html')
 
-# Load the trained model (ensure the path is correct)
-with open('predictions/models/dengue_model.pkl', 'rb') as file:
-    model = pickle.load(file)
+# Load the trained model and scaler (ensure paths are correct)
+model = joblib.load('predictions/models/random_forest_model.pkl')
+scaler = joblib.load('predictions/models/scaler.pkl')
 
 def predict_dengue(request):
+    probability = None
     prediction = None
-    
+
     if request.method == 'POST':
         # Get the form data
         heart_rate = float(request.POST.get('heart_rate', 0))
@@ -33,10 +35,26 @@ def predict_dengue(request):
         # Create the feature array for prediction
         input_features = np.array([[heart_rate, blood_pressure_systolic, blood_pressure_diastolic, haemoglobin, platelet_count, wbc_count, urea, sodium, potassium]])
 
-        # Make the prediction using the loaded model
-        prediction = model.predict(input_features)
-        prediction = "Dengue Positive" if prediction[0] == 1 else "Dengue Negative"
+       # Standardize the features using the same scaler
+        input_features = scaler.transform(input_features)
 
-    # Render the form with the prediction result (if any)
-    return render(request, 'predictions/index.html', {'prediction': prediction})
+        # Make the prediction using the loaded model
+        prediction_proba = model.predict_proba(input_features)
+        probability = prediction_proba[0][1]  # Probability of class 1 (Dengue Positive)
+        probability_percentage = probability * 100  # Convert probability to percentage
+        print(probability_percentage)
+        prediction = "Dengue Positive" if probability > 0.5 else "Dengue Negative"
+
+     # Convert probability to percentage
+        probability_percentage = round(probability * 100, 2)
+
+        # Return JSON response
+        return JsonResponse({
+            'prediction': prediction,
+            'probability_percentage': probability_percentage
+        })
+    
+    # Render the form if not POST request
+    return render(request, 'predictions/index.html')
+
 
